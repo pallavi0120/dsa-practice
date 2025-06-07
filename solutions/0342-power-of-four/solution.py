ebad153a9732eb35fa058b980aeed4f4fd50d6fc
unknown <pallavi200106@gmail.com>
2025-06-07 18:41:58 +0000

    [LeetCode Sync] Runtime - 1 ms (24.19%), Memory - 17.7 MB (100.00%)

---
+++ solutions/0342-power-of-four/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==0 or n<0:
+            return False
+        for i in range(n):
+            if n==4**(i):
+                return True
+            if n<4**(i):
+                return False
+
+        
