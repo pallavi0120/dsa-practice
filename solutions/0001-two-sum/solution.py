443bcb73331a3082457606e1f5b9ba3a6f540e9e
unknown <pallavi200106@gmail.com>
2026-02-02 23:53:42 +0000

    [LeetCode Sync] Runtime - 3 ms (53.72%), Memory - 20.5 MB (17.66%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,9 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output=[]
+        dic=dict()
+        op=[]
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    output.append(i)
-                    output.append(j)
-                    return output
+            if target-nums[i] not in dic.keys():
+                dic[nums[i]]=i
+            else:
+                j=dic[target-nums[i]]
+                op.append(j)
+                op.append(i)
+        return op
