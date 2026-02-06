58ddb7cf2331e59f2ade73c2629f5ca07ae2c298
unknown <pallavi200106@gmail.com>
2026-02-06 11:55:08 +0000

    [LeetCode Sync] Runtime - 3 ms (91.20%), Memory - 20 MB (98.82%)

--- solutions/0073-set-matrix-zeroes/solution.py
+++ solutions/0073-set-matrix-zeroes/solution.py
@@ -3,26 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
-        zero_positions = []
-
-        # Step 1: Store the positions of all zeroes
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j] == 0:
-                    zero_positions.append((i, j))
-
-        # Step 2: Zero the rows and columns for each zero position
-        for i, j in zero_positions:
-            self.func1(matrix, i)  # Set row to zero
-            self.func2(matrix, j)  # Set column to zero
-
-    def func1(self, matrix, row):
-        for j in range(len(matrix[0])):
-            matrix[row][j] = 0
-
-    def func2(self, matrix, col):
-        for i in range(len(matrix)):
-            matrix[i][col] = 0
-
+        n=len(matrix)
+        m=len(matrix[0])
+        col0=1
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    if j!=0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
+        if matrix[0][0]==0:
+            for j in range(m):
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(n):
+                matrix[i][0]=0
+        
