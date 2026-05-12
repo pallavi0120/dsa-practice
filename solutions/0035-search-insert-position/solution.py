bdc57073a40f6c6da27bfe3ea65d8e34f00c7a25
unknown <pallavi200106@gmail.com>
2026-05-12 15:49:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.9 MB (42.86%)

--- solutions/0035-search-insert-position/solution.py
+++ solutions/0035-search-insert-position/solution.py
@@ -1,8 +1,21 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        """
         if target in nums:
             return nums.index(target)
         else:
             nums.append(target)
             nums.sort()
             return nums.index(target)        
+            """
+        low=0
+        high=len(nums)-1
+        ans=len(nums)
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>=target:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
