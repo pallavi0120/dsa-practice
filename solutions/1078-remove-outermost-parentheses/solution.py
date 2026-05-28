8d9bc43c22594d0277a8343f3ad2713338cbbd84
unknown <pallavi200106@gmail.com>
2026-05-28 17:38:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (29.40%)

--- solutions/1078-remove-outermost-parentheses/solution.py
+++ solutions/1078-remove-outermost-parentheses/solution.py
@@ -1,24 +1,15 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        cl=0
-        cr=0
-        op=[]
-        sl=[]
-        l=list(s)
-        for i in range(len(l)):
-            if l[i]=='(':
-                cl+=1
-            else:
-                cr+=1
-            sl.append(l[i])
-            if cl==cr:
-                op.append(sl)
-                sl=[]
-        for i in range(len(op)):
-            op[i].pop()
-            op[i].pop(0)
+        n=len(s)
+        c=0
         res=''
-        for i in range(len(op)):
-            for j in range(len(op[i])):
-                res+=str(op[i][j])
+        for i in range(n):
+            if s[i]=='(':
+                c+=1
+                if c>1:
+                    res+='('
+            else:
+                c-=1
+                if c>0:
+                    res+=')'
         return res
