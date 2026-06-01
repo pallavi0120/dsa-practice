303be59eaf3d39b99df979479ab9fcd3df34460f
unknown <pallavi200106@gmail.com>
2026-06-01 10:25:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (84.29%)

--- solutions/1737-maximum-nesting-depth-of-the-parentheses/solution.py
+++ solutions/1737-maximum-nesting-depth-of-the-parentheses/solution.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def maxDepth(self, s: str) -> int:
         cl=0
         cr=0
@@ -9,4 +9,19 @@ class Solution:
             if s[i]==')':
                 cr+=1
             l.append(cl-cr)
-        return max(l)
+        return max(l)'''
+
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        ans=0
+        counter=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                counter+=1
+                #ans=max(ans,counter) or
+            elif s[i]==')':
+                counter-=1
+            else:
+                continue
+            ans=max(ans,counter)
+        return ans
