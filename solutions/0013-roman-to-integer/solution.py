9a0aed1ca825d0e441fcc40b375182c9d2aaa966
unknown <pallavi200106@gmail.com>
2026-06-01 10:43:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (18.99%)

--- solutions/0013-roman-to-integer/solution.py
+++ solutions/0013-roman-to-integer/solution.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def romanToInt(self, s: str) -> int:
         D={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         output=0
@@ -16,5 +16,15 @@ class Solution:
             output+=D[s[i]]
         #if i>len(s)-1:
             #break
-        return output
-
+        return output'''
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        D={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        res=0
+        for i in range(len(s)-1):
+            if D[s[i]]<D[s[i+1]]:
+                res-=D[s[i]]
+            else:
+                res+=D[s[i]]
+        res+=D[s[-1]]
+        return res
