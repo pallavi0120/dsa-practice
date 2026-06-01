11b5b99f3c7010d3b8bf7127d801e90da394968a
unknown <pallavi200106@gmail.com>
2026-06-01 08:22:10 +0000

    [LeetCode Sync] Runtime - 11 ms (77.15%), Memory - 19.4 MB (75.28%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -1,4 +1,17 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        return ''.join(sorted(s))==''.join(sorted(t))
-        
+'''class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1=list(s)
+        l2=[]
+        for i in l1:
+            l2.append()'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq=[0]*26
+        for i in s:
+            freq[ord(i)-ord('a')]+=1
+        for i in t:
+            freq[ord(i)-ord('a')]-=1
+        for count in freq:
+            if count!=0:
+                return False
+        return True
