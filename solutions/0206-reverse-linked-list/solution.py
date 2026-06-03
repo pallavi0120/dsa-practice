f27bd5bc65e4723462798cb9478df9bf16cb4068
unknown <pallavi200106@gmail.com>
2026-06-03 13:13:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (26.53%)

--- solutions/0206-reverse-linked-list/solution.py
+++ solutions/0206-reverse-linked-list/solution.py
@@ -3,7 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+'''class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev=None
         while head:
@@ -12,4 +12,13 @@ class Solution:
             prev=head
             head=current
         head=prev
-        return head
+        return head'''
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        newHead=self.reverseList(head.next)
+        front=head.next
+        front.next=head
+        head.next=None
+        return newHead
