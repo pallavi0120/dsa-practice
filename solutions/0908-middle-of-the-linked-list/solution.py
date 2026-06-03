404756d3a0d05b46371caccb2cd26e3ea363ceab
unknown <pallavi200106@gmail.com>
2026-06-03 12:02:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (57.46%)

--- solutions/0908-middle-of-the-linked-list/solution.py
+++ solutions/0908-middle-of-the-linked-list/solution.py
@@ -3,11 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+'''class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         l=[]
         while head:
             l.append(head)
             head=head.next
         m=l[len(l)//2]
-        return m
+        return m'''
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        fast=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        return slow
