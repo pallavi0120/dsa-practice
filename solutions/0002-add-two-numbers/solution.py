50b589b60b7ba158e40c0b1dabf7c3e371e02ffb
unknown <pallavi200106@gmail.com>
2026-06-04 17:29:09 +0000

    [LeetCode Sync] Runtime - 1 ms (75.45%), Memory - 19.4 MB (40.79%)

--- solutions/0002-add-two-numbers/solution.py
+++ solutions/0002-add-two-numbers/solution.py
@@ -3,7 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+'''class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         def extractnum(l):
             temp=l
@@ -20,8 +20,22 @@ class Solution:
         for i in range(1,len(s)):
             temp.next=ListNode(int(s[i]))
             temp=temp.next
-        return op
-
-
-
-
+        return op'''
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(-1)
+        temp=dummy
+        carry=0
+        while l1 or l2 or carry:
+            sum_val=0
+            if l1:
+                sum_val+=l1.val
+                l1=l1.next
+            if l2:
+                sum_val+=l2.val
+                l2=l2.next
+            sum_val+=carry
+            carry=sum_val//10
+            temp.next=ListNode(sum_val%10)
+            temp=temp.next
+        return dummy.next
