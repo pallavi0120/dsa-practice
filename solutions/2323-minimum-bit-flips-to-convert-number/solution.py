e7ff9d36da0a4a166aef307b7a9a269f9b5d56a4
unknown <pallavi200106@gmail.com>
2026-06-17 12:51:53 +0000

    [LeetCode Sync] Runtime - 3 ms (7.77%), Memory - 19.3 MB (62.80%)

---
+++ solutions/2323-minimum-bit-flips-to-convert-number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        diff=len(bin(start))-len(bin(goal))
+        if diff>0:
+            bg=(diff*'0')+bin(goal)[2:]
+            bs=bin(start)[2:]
+        elif diff<0:
+            bs=((-diff)*'0')+bin(start)[2:]
+            bg=bin(goal)[2:]
+        else:
+            bs=bin(start)[2:]
+            bg=bin(goal)[2:]
+        xorr=''
+        for i in range(len(bs)):
+            xorr+=str(int(bs[i])^(int(bg[i])))
+        return list(xorr).count('1')
