43d071decf75e954e9ccee83c8f53ad4008c098e
unknown <pallavi200106@gmail.com>
2026-06-25 10:24:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (27.88%)

--- solutions/0496-next-greater-element-i/solution.py
+++ solutions/0496-next-greater-element-i/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
         op=[]
         for i in range(len(nums1)):
             for j in range(nums2.index(nums1[i]),len(nums2)):
@@ -8,4 +9,19 @@ class Solution:
                     break
             if len(op)!=i+1:
                 op.append(-1)
+        return op'''
+        hashtable=[None]*(max(nums2)+1)
+        st=[]
+        n=len(nums2)
+        for i in range(len(nums2)-1,-1,-1):
+            while st and st[-1]<=nums2[i]:
+                st.pop()
+            if not st:
+                hashtable[nums2[i]]=-1
+            else:
+                hashtable[nums2[i]]=st[-1]
+            st.append(nums2[i])
+        op=[]
+        for i in nums1:
+            op.append(hashtable[i])
         return op
