5309f385c172da3992d059059fca7375aea86212
unknown <pallavi200106@gmail.com>
2026-06-26 06:17:26 +0000

    [LeetCode Sync] Runtime - 7 ms (57.85%), Memory - 20.3 MB (69.20%)

---
+++ solutions/0735-asteroid-collision/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        st=[]
+        for i in range(len(asteroids)):
+            if asteroids[i]>0:
+                st.append(asteroids[i])
+            else:
+                while st and st[-1]>0 and st[-1]<abs(asteroids[i]):
+                    st.pop()
+                if st and st[-1]==abs(asteroids[i]):
+                    st.pop()
+                elif not st or st[-1]<0:
+                    st.append(asteroids[i])
+                else:
+                    continue
+        return st
