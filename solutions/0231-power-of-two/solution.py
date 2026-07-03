5260b4f5b676401643434ec4d2f17a161ae1a7ae
unknown <pallavi200106@gmail.com>
2026-07-03 16:19:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (85.49%)

--- solutions/0231-power-of-two/solution.py
+++ solutions/0231-power-of-two/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        #if n!=1:
-            if n==0 or n<0:
-                return False
-            for i in range(n):
-                if n==2**(i):
-                    return True
-                if n<2**(i):
-                    return False
+        if n>0:
+            return n&(n-1)==0
+        else:
+            return False
+
