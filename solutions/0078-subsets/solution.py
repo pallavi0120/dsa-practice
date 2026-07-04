6265d3b6a67e647411952af6d829e01e7b27d541
unknown <pallavi200106@gmail.com>
2026-07-04 05:24:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (92.03%)

--- solutions/0078-subsets/solution.py
+++ solutions/0078-subsets/solution.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def f(self,i,arr,ds,res):
         #res.append(list(ds))
         if i>=len(arr):
@@ -11,4 +11,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ds,res=[],[]
         self.f(0,nums,ds,res)
-        return res
+        return res'''
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        subsets=1<<n
+        ans=[]
+        for num in range(subsets):
+            list=[]
+            for i in range(n):
+                if num&(1<<i):
+                    list.append(nums[i])
+            ans.append(list)
+        return ans
