6f4fe84cc9d549cfb5597ee4ff64fc836a8488cd
unknown <pallavi200106@gmail.com>
2026-07-04 12:36:02 +0000

    [LeetCode Sync] Runtime - 2 ms (77.56%), Memory - 20.5 MB (60.70%)

---
+++ solutions/0137-single-number-ii/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+        for i in range(1,len(nums),3):
+            if nums[i]!=nums[i-1]:
+                return nums[i-1]
+        return nums[n-1]
